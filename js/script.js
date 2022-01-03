0d101ef1f17377613be61d59b471ba744a3acfa9
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-01-03 18:15:47 +0100

    work on buttons

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 class BudgetApp {
-    constructor(){
+    constructor() {
         this.plusBtn = null;
         this.minusBtn = null;
-        this.addBtn= null;
+        this.addBtn = null;
 
-       this.UiSelectors = {
+        this.UiSelectors = {
             plusBtn: '[data-plusBtn]',
             minusBtn: '[data-minusBtn]',
-            addBtn: '[data-addBtn]'
+            addBtn: '[data-addBtn]',
+            descriptionField: '[data-description]',
+            valueField: '[data-value]',
         };
     }
-        initializeApp() {
-            this.plusBtn = document.querySelector(this.UiSelectors.plusBtn);
-            this.minusBtn = document.querySelector(this.UiSelectors.minusBtn);
-            this.addBtn = document.querySelector(this.UiSelectors.addBtn);
+    initializeApp() {
+        this.plusBtn = document.querySelector(this.UiSelectors.plusBtn);
+        this.minusBtn = document.querySelector(this.UiSelectors.minusBtn);
+        this.addBtn = document.querySelector(this.UiSelectors.addBtn);
+        this.descriptionField = document.querySelector(this.UiSelectors.descriptionField);
+        this.valueField = document.querySelector(this.UiSelectors.valueField);
 
-            console.log("sima");
-            this.eventListeners();
-        }
+        console.log("sima");
+        this.eventListeners();
+
+    }
 
     eventListeners() {
         this.plusBtn.addEventListener('click', () => this.plus());
@@ -25,26 +30,52 @@ class BudgetApp {
         this.addBtn.addEventListener('click', () => this.add());
     }
 
-    plus(){
+    //function to operate the "plus" button
+    plus() {
         console.log('Plus function');
         this.plusBtn.classList.toggle('hide');
         this.minusBtn.classList.toggle('hide');
     }
 
-    minus(){
+    // function to operate the "minus" button
+    minus() {
         console.log('Minus function');
         this.minusBtn.classList.toggle('hide');
         this.plusBtn.classList.toggle('hide');
     }
 
-    add(){
-        console.log("wartość = " + this.minusBtn.classList.value);
-
-        if(this.minusBtn.classList.value == 'sign sign__minus'){
-            console.log("sign minus");
-        }
-        else if(this.minusBtn.classList.value != 'sign sign__minus')  {
+    // Function to operate the "add" button.
+    add() {
+        if (this.minusBtn.className != 'sign sign__minus') {
             console.log("sign Plus");
+            this.addIncomes();
         }
+        // console.log("wartość = " + this.minusBtn.classList.value);
+        else if (this.minusBtn.className == 'sign sign__minus') {
+            console.log("sign minus");
+            this.addExpenses();
+        }
+
+        this.opis();
     }
+
+    //function to operate the "dexcription" button
+    opis() {
+        var description = this.descriptionField.value;
+        var value = this.valueField.value;
+
+        console.log(description + "  " + value);
+    }
+
+
+    //function to operate the "value" button
+
+
+
+    //The function of adding incomes to the list on the right
+    addIncomes() {}
+
+    // The function of adding expenses to the list on the right
+    addExpenses() {}
+
 }
