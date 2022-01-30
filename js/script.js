d5e46144e386d472cd6a27c19c208409ada217d7
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-01-30 22:45:54 +0100

    working on forEach function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 class BudgetApp {
-    balanceItems = [];
+    listOfItems = [];
     numberOfItems = 0;
 
     constructor() {
@@ -8,10 +8,9 @@ class BudgetApp {
         this.totalBudget = 0;
         this.id = 0;
         this.descriptionField = null;
-        var body = document.getElementsByTagName("body");
-        var bodycontent = body[0];
         this.valueField = 0;
         this.sum = 0;
+        this.listIncomes = "";
 
         this.UiSelectors = {
             operationBtn: '[data-operationBtn]',
@@ -21,8 +20,6 @@ class BudgetApp {
             announcement: '[data-announcement]',
 
             totalBudget: '[data-totalBudget]',
-            postedDesc: '[data-postedDesc]',
-            postedValue: '[data-postedValue]',
             listIncomes: '[data-listIncomes]',
             listExpenses: '[data-listExpenses]',
             lists: '[data-balance-list]',
@@ -35,8 +32,6 @@ class BudgetApp {
         this.operationBtn = document.querySelector(this.UiSelectors.operationBtn);
         this.addBtn = document.querySelector(this.UiSelectors.addBtn);
         this.descriptionField = document.querySelector(this.UiSelectors.descriptionField);
-        this.postedDesc = document.querySelector(this.UiSelectors.postedDesc);
-        this.postedValue = document.querySelector(this.UiSelectors.postedValue);
         this.valueField = document.querySelector(this.UiSelectors.valueField);
         this.totalBudget = document.querySelector(this.UiSelectors.totalBudget);
         this.announcement = document.querySelector(this.UiSelectors.announcement);
@@ -49,78 +44,33 @@ class BudgetApp {
         this.deleteBtn = document.querySelector(this.UiSelectors.deleteBtn);
 
         this.eventListeners();
-        this.getLocalStorage();
+        this.getLocalStorage();     
 
-        // this.balanceItems.forEach(({
-        //     id,
-        //     description,
-        //     value
-        // }) => {
-        //     this.listIncomes.insertAdjacentHTML('beforeend', this.createItem(id, description, value));
-        // });
-
-        this.balanceItems.forEach(({
-            id,
-            description,
-            value
-        }) => {
-            this.listExpenses.insertAdjacentHTML('beforeend', this.createItem(id, description, value));
-        });
-
-        // this.balanceItems.forEach(({ id, description, value, isPlus }) => {
-        //     isPlus
-        //       ? this.balanceListIncomes.insertAdjacentHTML(
-        //           'beforeend',
-        //           this.createItem(id, isPlus, description, value)
-        //         )
-        //       : this.balanceListExpenses.insertAdjacentHTML(
-        //           'beforeend',
-        //           this.createItem(id, isPlus, description, value)
-        //         );
-        //   });
     }
 
     eventListeners() {
         this.operationBtn.addEventListener('click', () => this.changeOperationBtn(this.operationBtn));
-        this.addBtn.addEventListener('click', () => this.add(this.id, this.descriptionField.value, this.valueField.value));
 
+        this.addBtn.addEventListener('click', () => this.addItem(this.id, this.descriptionField.value, this.valueField.value));
+
+        // Settings for Edit and Delete button in list Item
         this.lists.addEventListener('click', (e) => {
             this.clickF(e.target);
         })
 
+        //Adding to list by click enter
         document.addEventListener('keyup', (e) => {
-            console.log("pressed enter? " + e); ///?
+            console.log("pressed enter? " + e);
             if (e.code === 'Enter') {
-                this.add(this.id, this.descriptionField.value, this.valueField.value);
+                this.addItem(this.id, this.descriptionField.value, this.valueField.value);
             }
-        }, {once : true})
-
-        // this.listIncomes.addEventListener('click', function(e) {
-        //     console.log(e);
-        //     this.clickF(e.target);
-        // })   //czemu nie dziala?
+        }, {
+            once: true
+        })
 
     }
 
-    clickF(target) {
-        if (target.dataset && target.dataset.editBtn !== undefined) {
-            this.editItem(target);
-        }
-        console.log("loloo" + target.dataset);
-        if (target.dataset && target.dataset.deleteBtn !== undefined) {
-            // this.deleteItem()
-        }
-    }
-
-    editItem(target) {
-        const lis = target.parentElement.parentElement;
-        console.log(lis);
-
-        return {
-            element: lis
-        }
-    }
-
+    // change sign of button 
     changeOperationBtn(btn) {
         if (btn.classList.contains("sign__plus")) {
             btn.classList.remove("sign__plus")
@@ -133,9 +83,9 @@ class BudgetApp {
     }
 
     // Function to operate the "add" button.
-    add(id, description, value) {
+    addItem(id, description, value) {
 
-        this.balanceItems.push(this.getInputsValues());
+        this.listOfItems.push(this.getInputsValues());
 
         var num = parseFloat(value);
         if (description != '' && num != '') {
@@ -147,7 +97,7 @@ class BudgetApp {
                 ));
                 var numIncomes = num;
                 this.totalBudgetF(numIncomes, numExpenses);
-            }else if (this.operationBtn.classList.contains("sign__minus")) {
+            } else if (this.operationBtn.classList.contains("sign__minus")) {
                 this.listExpenses.insertAdjacentHTML('beforeend', this.createItem(
                     id,
                     description,
@@ -161,54 +111,77 @@ class BudgetApp {
         } else {
             this.announcement.innerHTML = "Fill in all fields";
         }
-      
+
         this.setlocalStorage();
     }
 
     setlocalStorage() {
-        localStorage.setItem('balanceItems', JSON.stringify(this.balanceItems));
+        localStorage.setItem('listOfItems', JSON.stringify(this.listOfItems));
         localStorage.setItem('numberOfItems', JSON.stringify(this.numberOfItems));
     }
 
     getLocalStorage() {
-        // this.balanceItems = JSON.parse(localStorage.getItem('balanceItems'))
-        // this.numberOfItems = JSON.parse(localStorage.getItem('numberOfItems'));
-        // console.log("Lists Items: " + balanceItems + "Number Of Items: " + this.numberOfItems);
+        this.listOfItems = localStorage.getItem('listOfItems')
+        if (this.listOfItems) {
+            this.listOfItems = JSON.parse(localStorage.getItem('listOfItems'))
+        } else {
+            this.listOfItems = [];
+        }
 
+        if (this.numberOfItems) {
+            this.numberOfItems = JSON.parse(localStorage.getItem('numberOfItems'))
+        } else {
+            this.numberOfItems = 0;
+        }
 
-        // this.balanceItems = localStorage.getItem('balanceItems')
-        // if(this.balanceItems){
-        //     this.balanceItems = JSON.parse(localStorage.getItem('balanceItems'))
-        // }
-        // else{
-        //     this.balanceItems = [];
-        // }
+        this.assignGetLocalStorage();
+    }
 
-        // if(this.numberOfItems){
-        //     this.numberOfItems = JSON.parse(localStorage.getItem('numberOfItems'))
-        // }else{
-        //     this.numberOfItems = 0;
-        // }
-
-            this.balanceItems = localStorage.getItem('balanceItems')
-            ? JSON.parse(localStorage.getItem('balanceItems'))
-            : [];
-            this.numberOfItems = localStorage.getItem('numberOfItems')
-            ? JSON.parse(localStorage.getItem('numberOfItems'))
-            : 0;
+    // Display on screen an earlier set Items
+    assignGetLocalStorage(){
+        this.listOfItems.forEach(
+            (item) => {
+                this.listIncomes.insertAdjacentHTML('beforeend', this.createItem(item.id, item.description, item.value));
+            });
+       
+        // this.listOfItems.forEach(({id, description, value}) => {
+        //     console.log("btn class list jest: " + this.operationBtn.classList);
+        //     if (this.operationBtn.classList.contains("sign__plus")) {
+        //         this.listExpenses.insertAdjacentHTML('beforeend', this.createItem(id, description, value));
+        //     } else {
+        //         this.listExpenses.insertAdjacentHTML('beforeend', this.createItem(id, description, value));
+        //     }
+        // });
     }
 
     // Create view single card 
-    createItem(id, postDesc, postValue) {
+    createItem(id, description, value) {
         return `
             <li class="list__item" id="${id}">
-                <span data-postedDesc> ${postDesc} </span>
-                <span data-postedValue> ${postValue} </span>
+                <span> ${description} </span>
+                <span > ${value} </span>
                     <div class="button">
                         <button class="button button__edit" data-editBtn> <i class="far fa-edit"></i> </button>
                         <button class="button button__trash" data-deleteBtn> <i class="fas fa-trash-alt"></i> </button>
                     </div>
-            </li>`;
+            </li>
+            `;
+    }
+
+    // <p class="item__value ${isPositive ? 'item__value--income' : 'item__value--expense'}" data-item-value>
+    // ${this.formatPrice(parseFloat(price), isPositive)} </p>
+
+    getInputsValues() {
+        const description = this.descriptionField.value;
+        const value = this.valueField.value;
+
+        if (value > 0 && description) {
+            return {
+                description,
+                value,
+            };
+        }
+        return null;
     }
 
     // Total budget available
@@ -221,9 +194,25 @@ class BudgetApp {
         this.totalBudget.innerHTML = this.sum;
     }
 
+    clickF(target) {
+        if (target.dataset && target.dataset.editBtn !== undefined) {
+            this.editItem(target);
+        }
+        console.log("loloo" + target.dataset);
+        if (target.dataset && target.dataset.deleteBtn !== undefined) {
+            // this.deleteItem()
+        }
+    }
 
     // Editing the created row
+    editItem(target) {
+        const lis = target.parentElement.parentElement;
+        console.log(lis);
 
+        return {
+            element: lis
+        }
+    }
 
     // Delete a row
     deleteItem(target) {
@@ -232,17 +221,6 @@ class BudgetApp {
         console.log(lis);
     }
 
-    getInputsValues() {
-        const description = this.descriptionField.value;
-        const value = this.valueField.value;
-
-        if (value > 0 && description) {
-            return {
-                description,
-                value,
-            };
-        }
-
-        return null;
-    }
+   
 }
+
